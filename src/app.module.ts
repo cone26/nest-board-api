e4ade78dd1a9a096b8158d50d422649ea8f84213
SiwonHwang <sight41siyah@gmail.com>
2022-11-24 22:51:08 +0900

    basic structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
+
+// module
+// Application Module <- User Module <- Feature Module 1
+//                    <- User Module <- Feature Module 2
+//                    <- User Module <- Feature Module 3
+
+// Application Module <- Orders Module
+// Application Module <- Chat Module
+
+// 관련이 있는 기능들을 집합으로 구성
+// 같은 기능을 사용하는 것들은 하나의 모듈 폴더안에 넣어서 사용 (UserController, UserService, UserEntity => UserModule)
+
+// module은 기본적으로 싱글톤이기 때문에 여러 모듈간에 쉽게 공급자의 동일한 인스턴스를 공유할 수 있다.
+// ex)
+// Users Module <- Shared Module
+// Orders Module <- Shared Module
+// Chat Module <- Shared Module
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+// 시작점 파일
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // root module인 app module을 생성
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
+
+// 실행 순서
+// main.ts -> app(AppModule)
+// app.module.ts -> controllers: [AppController], providers: [AppService]
+// app.controller.ts -> @Get() appService
+// app.service.ts -> getHello()
