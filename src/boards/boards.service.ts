3194a49b8639c6b6700e1abecfebd84f4216ad51
SiwonHwang <sight41siyah@gmail.com>
2022-11-24 22:56:25 +0900

    create a model file & use data structure in the module

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './boards.model';
 
 @Injectable()
 export class BoardsService {
   // 게시물 저장
-  private boards = [];
+  private boards: Board[] = [];
 
   // 모든 게시물 조회
-  getAllBoards() {
+  getAllBoards(): Board[] {
     return this.boards;
   }
 }
