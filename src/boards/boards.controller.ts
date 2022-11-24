099d560ba45e4f10a7b215b251206ec6755a9b1e
SiwonHwang <sight41siyah@gmail.com>
2022-11-24 22:54:28 +0900

    create a get handler : get all posts

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
+
+  //전체 게시물 조회
+  @Get()
+  getAllBoard() {
+    return this.boardsService.getAllBoards();
+  }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  // 게시물 저장
+  private boards = [];
+
+  // 모든 게시물 조회
+  getAllBoards() {
+    return this.boards;
+  }
+}
